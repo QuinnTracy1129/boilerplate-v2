772f3d4a5cf176be6ebb2c10e94ab731a07306ef
Earl Capuchino <benedictearle@gmail.com>
2023-12-12 15:38:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -33,10 +33,9 @@ require("./config/db")()
     app.use(
       express.urlencoded({
         extended: true,
-        limit: "50mb",
       })
     );
-    app.use(express.json({ limit: "50mb" }));
+    app.use(express.json());
     const storage = multer.memoryStorage();
     app.use(multer({ storage }).any());
 
